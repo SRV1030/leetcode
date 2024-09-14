5e945ad8b880deb72e7fc0951ac2fb0a702262cb
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2024-09-14 21:51:52 +0530

    Time: 6 ms (74.21%), Space: 7.7 MB (65.41%) - LeetHub

--- 1276-closest-divisors/1276-closest-divisors.cpp
+++ 1276-closest-divisors/1276-closest-divisors.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    vector<int> closestDivisors(int num) {
-   vector<int> ans;
-   long dif=INT_MAX;
-   for(int i=1;i*i<=num+2;++i){
-       if((num+1)%i==0){
-           int j= (num+1)/i;
-           if(abs(j-i)<dif){
-               dif=abs(j-i);
-               ans={i,j};
-           }
-       }
-       if((num+2)%i==0){
-           int j= (num+2)/i;
-           if(abs(j-i)<dif){
-               dif=abs(j-i);
-               ans={i,j};
-           }
-       }
-   }
-   return ans;
+    vector<int> closestDivisors(int x) {
+        for (int a = sqrt(x + 2); a > 0; --a) {
+            if ((x + 1) % a == 0)
+                return {a, (x + 1) / a};
+            if ((x + 2) % a == 0)
+                return {a, (x + 2) / a};
+        }
+        return {};
 }
 };
