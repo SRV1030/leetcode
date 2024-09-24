b99520bfca036fabeaf0221c7c7bd118e33ce656
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2024-09-24 17:45:15 +0530

    Time: 765 ms (29.93%), Space: 290.8 MB (49.46%) - LeetHub

---
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        set<string>st(begin(bannedWords),end(bannedWords));
+        int c=0;
+        for(auto&i:message){
+            if(st.count(i)) ++c;
+            if(c==2)return true;
+        }
+        return false;
+    }
+};
