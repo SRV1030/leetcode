e3b688979edd252ce36181f93c9f816587ee20a6
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2024-11-14 08:40:16 +0530

    Time: 24 ms (89.89%), Space: 87.3 MB (72.13%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
 
     bool isPoss(int n, vector<int>& qs,int k){
-        if(!k) return true;
         int c=0;
         for(auto&i:qs){
             c+=(i/k)+((i%k)!=0);
@@ -12,8 +11,6 @@ public:
     }
     int minimizedMaximum(int n, vector<int>& qs) {
         int l=1,h=*max_element(qs.begin(),qs.end()),ans=0;
-        if(n==1) return h;
-        if(qs.size()==1) return h;
         while(l<=h){
             int m = (l+h)>>1;
             if(isPoss(n,qs,m)){
