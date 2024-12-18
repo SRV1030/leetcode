2a9a547239cb6502995877b5a279897c36764e33
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2024-12-18 08:18:05 +0530

    Time: 3 ms (16%), Space: 14 MB (28.27%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -5,14 +5,10 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<prices.size();++i){
             while(!st.empty() && prices[st.top()]>=prices[i]){
-                mp[st.top()]=i;
+                prices[st.top()]-=prices[i];
                 st.pop();
             }
             st.push(i);
-        }
-        for(int i=0;i<prices.size();++i) {
-            if(mp.count(i))
-                prices[i]-=prices[mp[i]];
         }
         return prices;
     }
