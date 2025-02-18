025eef4be171ac55c9dd0c9a18e3ecf517d79f7b
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-02-18 16:09:48 +0530

    Time: 95 ms (19.71%), Space: 88.1 MB (20.63%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    vector<string>t;
-    bool isPal(string s,int i,int j){
-        while(i<=j){
+    bool isPal(string s, int i,int j){
+        while(i<j){
             if(s[i++]!=s[j--]) return false;
         }
         return true;
     }
-    void solve(string s,int i){
-        if(i==size(s)){
+    vector<vector<string>> ans;
+    vector<string> t;
+    int n;
+    void applyPartion(string s,int i){
+        if(i==n){
             ans.push_back(t);
+            return;
         }
-        else{
-            for(int j=i;j<=size(s);++j){
-                if(isPal(s,i,j)){
-                    t.push_back(s.substr(i,j-i+1));
-                    solve(s,j+1);
-                    t.pop_back();
-                }
+        for(int j=i;j<n;++j){
+            if(isPal(s,i,j)){
+                t.push_back(s.substr(i,j-i+1));
+                applyPartion(s,j+1);
+                t.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        solve(s,0);
+        n=s.size();
+        applyPartion(s,0);
         return ans;
     }
 };
