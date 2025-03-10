98ab4d349ec6943a6d75d7e366d4346c083dd8a6
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-03-10 09:31:13 +0530

    Time: 182 ms (66.58%), Space: 48.8 MB (53.15%) - LeetHub

---
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isVowel(char x){
+        return x=='a' || x=='e' || x=='i' || x=='o' || x=='u';
+    }
+    long long atLeastK(string word, int k){
+        long numValidSubstrings = 0;
+        int start = 0;
+        int end = 0;
+        // Keep track of counts of vowels and consonants.
+        unordered_map<char, int> vowelCount;
+        int consonantCount = 0;
+
+        // Start sliding window.
+        while (end < word.length()) {
+            // Insert new letter.
+            char newLetter = word[end];
+
+            // Update counts.
+            if (isVowel(newLetter)) {
+                vowelCount[newLetter]++;
+            } else {
+                consonantCount++;
+            }
+
+            // Shrink window while we have a valid substring.
+            while (vowelCount.size() == 5 and consonantCount >= k) {
+                numValidSubstrings += word.length() - end;
+                char startLetter = word[start];
+                if (isVowel(startLetter)) {
+                    if (--vowelCount[startLetter] == 0) {
+                        vowelCount.erase(startLetter);
+                    }
+                } else {
+                    consonantCount--;
+                }
+                start++;
+            }
+
+            end++;
+        }
+
+        return numValidSubstrings;
+    }
+    long long countOfSubstrings(string word, int k) {
+        return atLeastK(word,k)-atLeastK(word,k+1);
+    }
+};
