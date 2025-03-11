7d7b485271f6435ea5c7a9a08ad630c64970605a
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-03-11 15:29:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (56.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1 || n==2 || n==3) return n;
-        int a=2,b=3,c=3;
-        for(int i=4;i<=n;i++ ){
+        if(n<=2) return n;
+        int a = 1,b = 2,c=0;
+        for(int i=3;i<=n;i++){
             c=a+b;
             a=b;
             b=c;
