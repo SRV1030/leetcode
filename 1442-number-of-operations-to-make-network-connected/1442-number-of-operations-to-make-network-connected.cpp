c3ec46a0f62786686b919613ddd96ed1016ff224
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-03-17 08:18:40 +0530

    Time: 2 ms (90.16%), Space: 42.1 MB (93.42%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    vector<int> group,rank;
-    int getGroup(int component){
-        while(component!=group[component]) 
-            return group[component]=getGroup(group[component]);
+    vector<int> group, rank;
+    int getGroup(int component) {
+        while (component != group[component])
+            return group[component] = getGroup(group[component]);
         return component;
     }
-    void connect(int u,int v){
-        int g1=getGroup(u),g2=getGroup(v);
-        if(g1!=g2){
-            if(rank[g1]<rank[g2]){
-                group[g1]=g2;
-                rank[g2]+=rank[g1];
-            }
-            else{
-                group[g2]=g1;
-                rank[g1]+=rank[g2];
+    void connect(int u, int v) {
+        int g1 = getGroup(u), g2 = getGroup(v);
+        if (g1 != g2) {
+            if (rank[g1] < rank[g2]) {
+                group[g1] = g2;
+                rank[g2] += rank[g1];
+            } else {
+                group[g2] = g1;
+                rank[g1] += rank[g2];
             }
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1) return -1;
+        if (connections.size() < n - 1)
+            return -1;
         group.resize(n);
         rank.resize(n);
-        iota(group.begin(),group.end(),0);
-        for(auto&component:connections)connect(component[0],component[1]);
+        iota(group.begin(), group.end(), 0);
+        for (auto& component : connections)
+            connect(component[0], component[1]);
         int disconnectedComponents = 0;
-        for(int i=0;i<n;i++)
-            if(group[i]==i) ++disconnectedComponents;
-        return disconnectedComponents-1;
+        for (int i = 0; i < n; i++)
+            if (group[i] == i)
+                ++disconnectedComponents;
+        return disconnectedComponents - 1;
     }
 };
