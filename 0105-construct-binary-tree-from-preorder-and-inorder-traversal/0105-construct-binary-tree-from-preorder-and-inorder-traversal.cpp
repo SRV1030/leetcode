af23fd3f7426737f447520526326929c75603a4a
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-03-26 19:25:35 +0530

    Time: 3 ms (74.68%), Space: 27 MB (96.94%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,27 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
+
 public:
-    int preInd=0;
-    int search(vector<int>& in,int x,int l,int h){
-        for(int i=l;i<=h;i++) if(in[i]==x) return i;
+int preInd=0;
+    int findInorder(int l, int h, int val, vector<int>& inorder) {
+        for (int i = l; i <= h; i++)
+            if (inorder[i] == val)
+                return i;
         return -1;
     }
-    TreeNode* build(vector<int>& pre, vector<int>& in,int l,int h){
-       if(l>h) return NULL;
-        TreeNode *root=new TreeNode(pre[preInd++]);
-        int i=search(in,root->val,l,h);
-        if(i!=-1){
-            root->left=build(pre,in,l,i-1);
-            root->right=build(pre,in,i+1,h);
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int l, int h) {
+        if(l>h) return NULL;
+        TreeNode *newNode= new TreeNode(preorder[preInd++]);
+        int ind=findInorder(l,h,newNode->val,inorder);
+        if(ind!=-1){
+            newNode->left=build(preorder, inorder, l, ind-1);
+            newNode->right=build(preorder, inorder, ind+1,h);
         }
-        return root;
+        return newNode;
     }
-    TreeNode* buildTree(vector<int>& p, vector<int>& in) {
-        return build(p,in,0,p.size()-1);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return build(preorder, inorder, 0, inorder.size() - 1);
     }
 };
