b9a939cb26f8374d3e29ba72ba88f0d3169714c8
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-03-27 09:28:49 +0530

    Time: 0 ms (100%), Space: 89.8 MB (85.66%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> fmp, tfmp;
+        int xCount = 0, x = nums[0], n = nums.size();
+        for (auto& i : nums) {
+            if (i == x)
+                xCount++;
+            else
+                xCount--;
+            if (xCount == 0) {
+                x = i;
+                xCount = 1;
+            }
+        }
+        xCount = 0;
+        int count = 0;
         for (auto& i : nums)
-            fmp[i]++;
+            xCount += (i == x);
         for (int i = 0; i < n; i++) {
-            tfmp[nums[i]]++;
-            if ((tfmp[nums[i]] * 2) > (i + 1) &&
-                (((fmp[nums[i]] - tfmp[nums[i]]) * 2 )> (n - i - 1)))
-                return i;
+            if (nums[i] == x) {
+                ++count;
+                if (count * 2 > (i + 1) && (xCount - count) * 2 > (n - i - 1))
+                    return i;
+            }
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int minimumIndex(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int, int> fmp, tfmp;
+//         for (auto& i : nums)
+//             fmp[i]++;
+//         for (int i = 0; i < n; i++) {
+//             tfmp[nums[i]]++;
+//             if ((tfmp[nums[i]] * 2) > (i + 1) &&
+//                 (((fmp[nums[i]] - tfmp[nums[i]]) * 2 )> (n - i - 1)))
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
