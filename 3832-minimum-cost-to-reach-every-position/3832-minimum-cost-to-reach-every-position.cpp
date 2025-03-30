2b173ff25d18da17dcd636eb44b1e38651a99356
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-03-30 14:41:33 +0530

    Time: 0 ms (100%), Space: 26.5 MB (92.32%) - LeetHub

--- 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
+++ 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     vector<int> minCosts(vector<int>& cost) {
         int size = cost.size();
-        vector<int> answer(size, -1);
-        vector<vector<int>> costInd;
-        for (int i = 0; i < size; i++)
-            costInd.push_back({cost[i], i});
-        sort(costInd.begin(), costInd.end());
-        for (auto& i : costInd) {
-            int index = i[1], value = i[0];
-            if (answer[index] != -1)continue;
-            for (; index < size && answer[index]==-1; index++)
-                answer[index] = value;
+        vector<int> answer(size);
+        int minCost=cost[0];
+        for (int i = 0; i < size; i++){
+            minCost=min(minCost,cost[i]);
+            answer[i]=minCost;
         }
         return answer;
     }
