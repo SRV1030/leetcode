684c7854937a9d2541afeb1bbb7b4c59a9b56b90
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-04 12:58:50 +0530

    Time: 22 ms (69.27%), Space: 12.3 MB (99.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int dp[13][10001];
-    int solve(vector<int>& cs, int at,int i){
-        if(at==0) return 0;
-        if(i==cs.size() || at<0) return INT_MAX;
-        if(dp[i][at]!=-1) return dp[i][at];
-        int take = INT_MAX;
-        if(cs[i]>0){
-            take=solve(cs,at-cs[i],i);
-            if(take!=INT_MAX)take++;
-        }
-        int notTake = solve(cs,at,i+1);
-        return dp[i][at]=min(take,notTake); 
-    }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int ans = solve(coins,amount,0);
-        if(ans==INT_MAX) ans=-1;
-        return ans;
+        sort(coins.begin(),coins.end());
+        ++amount;
+        int dp[amount];
+        dp[0]=0;
+        for(int i=1;i<amount;i++){
+            dp[i]=INT_MAX;
+            for(auto&c:coins){
+                if(i-c<0) break;
+               if(dp[i-c]!=INT_MAX) dp[i]=min(dp[i],1+dp[i-c]);
+            }
+        }
+        return dp[--amount]==INT_MAX?-1:dp[amount];
     }
 };
