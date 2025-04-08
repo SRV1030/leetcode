357b0c16aaf63fbf9500318a3a0966a51b5cad97
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-08 07:06:40 +0530

    Time: 15 ms (19.11%), Space: 36.7 MB (13.65%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    bool isUnique(unordered_map<int,int>mp){
-        for(auto&[k,v]:mp)
-            if(v>1) return false;
+    bool isUnique(vector<int> nums){
+        unordered_set<int>seen;
+        for(auto&i:nums)
+            if(!seen.insert(i).second) return false;
         return true;
     }
     int minimumOperations(vector<int> nums) {
-        unordered_map<int,int>mp;
-        for(auto&i:nums)mp[i]++;
-        if(isUnique(mp) || nums.size()==0) return 0;
+        if(nums.size()==0 || isUnique(nums)) return 0;
         if(nums.size()<=3) return 1;
         return 1+minimumOperations(vector<int>(nums.begin()+3,nums.end()));
     }
