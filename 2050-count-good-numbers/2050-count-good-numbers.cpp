3e617467b9b2dd173e072f0e0d46e10b1bd900d3
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-13 11:43:25 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.45%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int MOD = 1e9+7;
-    long long pow(long long a,long long b){
-        if(!b) return 1;
+    int MOD = 1e9 + 7;
+    long long pow(long long a, long long b) {
+        if (!b)
+            return 1;
         long long res = 1;
-        while(b>0){
-            if(b%2)res = (res*a)%MOD;
-            a = (a*a)%MOD;
-            b>>=1;
+        while (b > 0) {
+            if (b % 2)
+                res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
         }
-        return res%MOD;
+        return res % MOD;
     }
     int countGoodNumbers(long long n) {
-        if(n==1) return 5;
-        long long half = n/2;
-        return( pow(5,n-half)*pow(4,half))%MOD;
+        if (n == 1)
+            return 5;
+        long long half = n / 2;
+        return (pow(5, n - half) * pow(4, half)) % MOD;
     }
 };
