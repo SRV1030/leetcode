e66cdb40de9c20a7d5bd7af12f68e246ca7a6af5
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-17 13:26:28 +0530

    Time: 19 ms (18.92%), Space: 18.6 MB (17.93%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     set<string>dictionary;
-    int dp[301][301];
-    bool segmentCheck(string s,int lo,int hi){
-        if(lo>hi) return true;
-        int &ans = dp[lo][hi];
+    int dp[301];
+    bool segmentCheck(string s,int lo){
+        if(lo>=s.size()) return true;
+        int &ans = dp[lo];
         if(ans!=-1) return ans;
-        for(int j=lo;j<=hi;j++){
-            if(dictionary.count(s.substr(lo,j-lo+1)) && segmentCheck(s,j+1,hi)) return ans = 1; 
+        for(int j=lo;j<s.size();j++){
+            if(dictionary.count(s.substr(lo,j-lo+1)) && segmentCheck(s,j+1)) return ans = 1; 
         }
         return ans=0;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         dictionary=set<string>(wordDict.begin(),wordDict.end());
         memset(dp,-1,sizeof(dp));
-        return segmentCheck(s,0,s.size()-1);
+        return segmentCheck(s,0);
     }
 };
