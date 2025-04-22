0f89a3dcdcee5674b408bfa72fdffc933aa2fbe0
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-22 17:46:19 +0545

    Time: 0 ms (100%), Space: 11 MB (28.98%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void solve(vector<int>nums,int l,int r){
-        if(l==r) ans.push_back(nums);
+    vector<vector<int>> allPermutations;
+    int high;
+    void generateAllPermutations(vector<int>& nums,int low){
+        if(low==high)allPermutations.push_back(nums);
         else{
-            for(int i=l;i<=r;++i){
-                swap(nums[i],nums[l]);
-                solve(nums,l+1,r);
-                swap(nums[i],nums[l]);
+            for(int i=low;i<=high;i++){
+                swap(nums[i],nums[low]);
+                generateAllPermutations(nums,low+1);
+                swap(nums[i],nums[low]);
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        solve(nums,0,size(nums)-1);
-        return ans;
+        high = nums.size()-1;
+        generateAllPermutations(nums,0);
+        return allPermutations;
     }
 };
