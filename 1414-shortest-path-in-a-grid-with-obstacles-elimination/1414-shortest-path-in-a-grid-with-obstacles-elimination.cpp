4285c09f3cce65c06f7d9800c2ae718a3a3ea840
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-23 14:11:24 +0545

    Time: 145 ms (39.4%), Space: 55 MB (39.6%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,int k){
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
-        queue<vector<int>> q;
-	
-        q.push({0,0,0,k});
-        while(!q.empty()){
-            auto t=q.front();
-            int x=t[0],y=t[1];
-            q.pop();
-			
-			// Exit if current position is outside of the grid
-            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
-                continue;
-            }
-			
-			// Destination found
-            if(x==grid.size()-1 && y==grid[0].size()-1)
-                return t[2];
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int rows = grid.size();
+        int columns = grid[0].size();
+        vector<vector<int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        vector<vector<int>> visited(rows, vector<int>(columns, -1));
+        queue<vector<int>> simulationQueue;
 
-            if(grid[x][y]==1){
-                if(t[3]>0)
-                    t[3]--;
+        simulationQueue.push({0, 0, 0, k});
+
+        while (!simulationQueue.empty()) {
+            auto currentStats = simulationQueue.front();
+            simulationQueue.pop();
+
+            int x = currentStats[0], y = currentStats[1],
+                distanceCovered = currentStats[2], kValue = currentStats[3];
+
+            if (min(x, y) < 0 || x >= rows || y >= columns)
+                continue;
+
+            if (x == rows - 1 && y == columns - 1)
+                return distanceCovered;
+
+            if (grid[x][y]) {
+                if (kValue>0)
+                    kValue--;
                 else
                     continue;
             }
-			
 
-            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
+            if (visited[x][y] != -1 && visited[x][y] >= kValue)
                 continue;
-            vis[x][y]=t[3];
-            
-            q.push({x+1,y,t[2]+1,t[3]});
-            q.push({x,y+1,t[2]+1,t[3]});
-            q.push({x-1,y,t[2]+1,t[3]});
-            q.push({x,y-1,t[2]+1,t[3]});
-            
+
+            visited[x][y] = kValue;
+
+            for (auto& direction : directions)
+                simulationQueue.push({x + direction[0], y + direction[1],
+                                      1 + distanceCovered, kValue});
         }
         return -1;
-    }
-    
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        return solve(grid,k);
     }
 };
