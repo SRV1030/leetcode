8a32ce86afb4ba6037025352689694f06fb1420d
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-04-26 08:50:05 +0545

    Time: 63 ms (32.29%), Space: 15 MB (81.97%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int r, c;
-    int dp[71][71][71];
-    int solve(int i, int j1, int j2, vector<vector<int>>& grid) {
-        if (i >= r || min(j1, j2) < 0 || max(j1, j2) >= c)
-            return INT_MIN;
-
-        int& ans = dp[i][j1][j2];
-        if (ans != -1)
-            return ans;
-
-        int value = j1 == j2 ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-        ans = value;
-        if (i == r - 1)
-            return ans;
-
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                int nextVal = solve(i + 1, j1 + x, j2 + y, grid);
-                if (nextVal != INT_MIN) {
-                    ans = max(ans, nextVal + value);
-                }
+    int rowSize,colSize;
+    int memory[71][71][71];
+    int pickMaxCherries(int row1,int col1,int col2,vector<vector<int>>& grid){
+        if(min({row1,col1,col2})<0 || row1>=rowSize || max(col1,col2)>=colSize) return INT_MIN;
+        int &cherries = memory[row1][col1][col2];
+        if(cherries!=-1) return cherries;
+        cherries = col1==col2?grid[row1][col1]:grid[row1][col1]+grid[row1][col2];
+        if(row1==rowSize) return cherries;
+        int nextMaxPick = 0;
+        for(int i=-1;i<=1;i++)
+            for(int j=-1;j<=1;j++){
+                nextMaxPick = max(nextMaxPick,pickMaxCherries(row1+1,col1+i,col2+j,grid));
             }
-        }
-        return ans;
+        cherries+=nextMaxPick;
+        return cherries;
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        r = grid.size();
-        c = grid[0].size();
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, c - 1, grid);
+        rowSize = grid.size();
+        colSize = grid[0].size();
+        memset(memory,-1,sizeof(memory));
+        return pickMaxCherries(0,0,colSize-1,grid);
     }
 };
