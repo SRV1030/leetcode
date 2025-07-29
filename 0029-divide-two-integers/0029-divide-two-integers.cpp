494f92fb2e0d6517fbcaa30ba39961197635898b
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-07-29 10:00:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.49%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int divide(long long dividend, long long divisor) {
+        if(dividend == divisor)
+            return 1;
+        if((divisor==-1 && dividend==(1<<31)))return INT_MAX;
+        bool sign = (dividend < 0) == (divisor < 0);
+        long long quotient = 0;
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+        while(dividend >= divisor){
+            int bit = 0;
+            while(dividend > (divisor << (bit + 1)))
+                ++bit;
+            quotient += (1 << bit);
+            dividend -= (divisor << bit);
+        }
+        if(quotient == (1 << 31) && sign)
+            return INT_MAX;
+        return sign ? quotient : -quotient;
+    }
+};
