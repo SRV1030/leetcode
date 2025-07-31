4455d6e05c5f6fcc8b22ffbc4bfb75030a9f307d
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-07-31 16:54:16 +0530

    Time: 1432 ms (9.76%), Space: 338.6 MB (48.63%) - LeetHub

---
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        set<int> resultOrs, previousOrs;
+        for(auto& num: arr){
+            set<int> runningOrs;
+            runningOrs.insert(num);
+            for(auto& ors: previousOrs)
+                runningOrs.insert(num | ors);
+            resultOrs.insert(runningOrs.begin(), runningOrs.end());
+            previousOrs = runningOrs;
+        }
+        return resultOrs.size();
+    }
+};
