07c839b568587314d810ba0306e7b442dedb89b2
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-08-20 09:45:22 +0530

    Time: 2 ms (92%), Space: 8.4 MB (92.35%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
+    void countPalindrome(int index, string& s, int offset, int& count){
+        int left = index, right = index + offset;
+        while(left >= 0 && right < s.size() && s[left--] == s[right++]){
+            ++count;
+        }
+    }
+    int countSubstrings(string& s) {
         int count = 0;
         for(int index = 0; index < s.size(); ++index){
-            int left = index, right = index;
-            while(left >= 0 && right < s.size() && s[left--] == s[right++]){
-                ++count;
-            }
-            left = index, right = index + 1;
-            while(left >= 0 && right < s.size() && s[left--] == s[right++]){
-                ++count;
-            }
+            countPalindrome(index, s, 0, count);
+            countPalindrome(index, s, 1, count);
         }
         return count;
     }
