68a9b408b6c2662783b0bcd61d5f8c5c52be501e
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-08-30 10:54:56 +0530

    Time: 0 ms (100%), Space: 21.5 MB (91.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char>> rows(9), cols(9), block(9);
+        bitset<9> rows[9], cols[9], block[9];
         for(int row = 0; row < 9; ++row){
             for(int col = 0; col < 9; ++col){
                 if(board[row][col] != '.'){
                     int blockIndex = row / 3 * 3 + col / 3;
-                    if(rows[row].count(board[row][col]) || cols[col].count(board[row][col]) || block[blockIndex].count(board[row][col]))
+                    int numIndex = board[row][col] - '0';
+                    if(rows[row][numIndex] || cols[col][numIndex] || block[blockIndex][numIndex])
                         return false;
-                    rows[row].insert(board[row][col]);
-                    cols[col].insert(board[row][col]);
-                    block[blockIndex].insert(board[row][col]);
+                    rows[row][numIndex] = 1;
+                    cols[col][numIndex] = 1;
+                    block[blockIndex][numIndex] = 1;
                 }
             }
         }
