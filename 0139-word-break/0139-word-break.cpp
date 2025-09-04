5e6b4ca8b2558c5bea39e071e665f15e5cc8757f
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-09-04 08:50:43 +0530

    Time: 43 ms (5.11%), Space: 27.2 MB (5.06%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,19 +1,79 @@
 class Solution {
-public:
-    set<string>dictionary;
-    int dp[301];
-    bool segmentCheck(string s,int lo){
-        if(lo>=s.size()) return true;
-        int &ans = dp[lo];
-        if(ans!=-1) return ans;
-        for(int j=lo;j<s.size();j++){
-            if(dictionary.count(s.substr(lo,j-lo+1)) && segmentCheck(s,j+1)) return ans = 1; 
+    class Node{
+        vector<Node*> children;
+        bool isEnd;
+    public:
+        Node(){
+            children = vector<Node*> (26);
+            isEnd = false;
         }
-        return ans=0;
+        
+        void setIsEnd(){
+            isEnd = true;
+        }
+
+        bool getIsEnd(){
+            return isEnd;
+        }
+
+        Node* putChar(char x){
+            int charIndex= x - 'a';
+            if(!children[charIndex])
+                children[charIndex] = new Node();
+            return children[charIndex];
+        }
+
+        Node* gotoChar(char x){
+            return children[x - 'a'];
+        }
+    };
+    class Trie{
+        Node* root;
+    public:
+        Trie(){
+            root = new Node();
+        }
+
+        void insert(string word){
+            Node* node = root;
+            for(auto& character : word){
+                node = node->putChar(character);
+            }
+            node->setIsEnd();
+        }
+
+        bool search(string word){
+            Node* node = root;
+            for(auto& character : word){
+                node = node->putChar(character);
+                if(!node)
+                    return false;
+            }
+            return node->getIsEnd();
+        }
+    };
+    vector<int> memory;
+    int size;
+    Trie* trie;
+public:
+    bool segmentCheck(string s, int index){
+        if(index >= size)
+            return true;
+        if(memory[index] != -1)
+            return memory[index];
+        for(int low = index; low < size; ++low){
+            if(trie->search(s.substr(index, low - index + 1)) && segmentCheck(s, low + 1))
+                return memory[index] = 1;
+        }
+        return memory[index] = 0;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        dictionary=set<string>(wordDict.begin(),wordDict.end());
-        memset(dp,-1,sizeof(dp));
-        return segmentCheck(s,0);
+        size = s.size();
+        memory = vector<int> (size, -1);
+        trie = new Trie();
+        for(auto& word : wordDict){
+            trie->insert(word);
+        }
+        return segmentCheck(s, 0);
     }
 };
