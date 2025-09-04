85e6d29a01841443e321c9c15f707550b1997c7c
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-09-04 09:18:01 +0530

    Time: 0 ms (100%), Space: 9.2 MB (69.6%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -15,14 +15,7 @@ public:
         return count;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned long long> memory (target + 1);
-        memory[0] = 1;
-        for(int temp = 1; temp <= target; ++temp){
-            for(auto& num : nums){
-                if(num <= temp)
-                    memory[temp] += memory[temp - num];
-            }
-        }
-        return memory[target];
+        memory = vector<int> (target + 1, -1);
+        return countCombos(nums, target);
     }
 };
