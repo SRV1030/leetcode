01a1c65f16985c1f8da5d64c109946c92179d4be
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-09-21 12:06:37 +0530

    Time: 0 ms (100%), Space: 77.2 MB (13.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,43 +10,33 @@
  */
 class Solution {
 public:
-    // ListNode* rev(ListNode* h){
-    //     if(!h) return;
-    //     ListNode* cur=h,nxt=NULL,prev=NULL;
-    //     while(cur){
-    //         nxt=cur->next;
-    //         cur->next=prev;
-    //         prev=cur;
-    //         cur=nxt;
-    //     }
-    //     return prev;
-    // }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* cur=new ListNode(0), *ans=cur;
-        int c=0;
+        ListNode* resultHead = new ListNode(0), *current = resultHead;
+        int c = 0;
         while(l1 && l2){
-            int v=l1->val+l2->val+c;
-            c=v/10;
-            cur->next=new ListNode(v%10);
-            cur=cur->next;
-            l1=l1->next;
-            l2=l2->next;
+            int sum = l1->val + l2->val + c;
+            c = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            l1 = l1->next;
+            l2 = l2->next;
         }
         while(l1){
-            int v=l1->val+c;
-            c=v/10;
-            cur->next=new ListNode(v%10);
-            cur=cur->next;
-            l1=l1->next;
+            int sum = l1->val + c;
+            c = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            l1 = l1->next;
         }
         while(l2){
-            int v=l2->val+c;
-            c=v/10;
-            cur->next=new ListNode(v%10);
-            cur=cur->next;
-            l2=l2->next;
+            int sum = l2->val + c;
+            c = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            l2 = l2->next;
         }
-        if(c)cur->next=new ListNode(c);
-        return ans->next;
+        if(c)
+            current->next = new ListNode(c);
+        return resultHead->next;
     }
 };
