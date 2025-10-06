674bba9a073b4d93fda2d3ce7e90c17c439efdd5
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-10-06 10:17:44 +0530

    Time: 0 ms (100%), Space: 76.8 MB (81.61%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,9 +10,9 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head){
-        ListNode* current = head, *next = NULL, *prev = NULL;
-        while(current){
+    ListNode* reverse(ListNode* head) {
+        ListNode* current = head, *next = nullptr, *prev = nullptr;
+        while (current) {
             next = current->next;
             current->next = prev;
             prev = current;
@@ -20,36 +20,35 @@ public:
         }
         return prev;
     }
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* r1 = reverse(l1), *r2 = reverse(l2);
         ListNode* sum = new ListNode(0), *sumHead = sum;
         int carry = 0;
-        while(r1 && r2){
-            int value = (r1->val + r2->val + carry);
+
+        // \U0001f539 Main loop: process both lists
+        while (r1 || r2 || carry) {
+            int value = carry;  // start with carry
+
+            // \U0001f539 Add node values if present
+            if (r1) {
+                value += r1->val;
+                r1 = r1->next;
+            }
+            if (r2) {
+                value += r2->val;
+                r2 = r2->next;
+            }
+
+            // \U0001f539 fix: compute digit = value % 10, carry = value / 10
             sumHead->next = new ListNode(value % 10);
             carry = value / 10;
-            r1= r1->next;
-            r2 = r2->next;
+
             sumHead = sumHead->next;
         }
-        while(r1){
-            int value = (r1->val + carry);
-            sumHead->next = new ListNode(value % 10);
-            carry = value / 10;
-            r1= r1->next;
-            sumHead = sumHead->next;
-        }
-        while(r2){
-            int value = (r2->val + carry);
-            sumHead->next = new ListNode(value % 10);
-            carry = value / 10;
-            r2= r2->next;
-            sumHead = sumHead->next;
-        }
-        if(carry){
-            sumHead->next = new ListNode(carry);
-        }
-        ListNode* reslt = sum->next;
-        return reverse(reslt);
+
+        // \U0001f539 reverse result before returning
+        ListNode* result = reverse(sum->next);
+        return result;
     }
 };
