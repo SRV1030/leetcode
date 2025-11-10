4e274f6c56669664427a806142824929ff6a7957
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-11-10 09:52:05 +0530

    Time: 0 ms (100%), Space: 7.8 MB (54.33%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long low = 0, high = sqrt(c);
+        while(low <= high){
+            long long sum = low * low + high * high;
+            if(sum == c)
+                return true;
+            if(sum > c)
+                high--;
+            else 
+                low++;
+        }
+        return false;
+    }
+};
