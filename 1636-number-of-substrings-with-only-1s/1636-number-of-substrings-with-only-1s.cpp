621d22770d97783f309192acbbf6ac4518a81d96
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-11-16 17:57:11 +0530

    Time: 3 ms (33.16%), Space: 11.6 MB (39.33%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -3,16 +3,13 @@ public:
     int numSub(string s) {
         long long oc = 0, res = 0, mod = 1e9 + 7;
         for(int right = 0; right < s.size(); ++right){
-            if(s[right] == '1')
+            if(s[right] == '1'){
                 oc = (oc + 1) % mod;
-            else{
-                res += (oc * (oc + 1)) / 2;
-                res %= mod;
-                oc = 0;
+                res = (oc + res) % mod;
             }
+            else
+                oc = 0;
         }
-        if(oc)
-            res += (oc * (oc + 1)) / 2;
         return res % mod;
     }
 };
