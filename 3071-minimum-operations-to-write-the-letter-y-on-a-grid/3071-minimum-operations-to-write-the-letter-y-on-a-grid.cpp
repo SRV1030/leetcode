4ef0d9ee4a03c7d7c39105cf2c531e725b7a7436
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-11-24 15:38:03 +0530

    Time: 0 ms (100%), Space: 41.8 MB (73.13%) - LeetHub

---
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool isOnY(int &size, int &row, int &col, int &center){
+        return (row == col && row <= center) || ((row + col + 1) == size && row <= center) || (row > center && col ==center);
+    }
+public:
+    int minimumOperationsToWriteY(vector<vector<int>>& grid) {
+        int size = grid.size();
+        int center = size / 2, ySize = 3 * center, rest = size * size - ySize;
+        vector<int> yFrequency(3), nonYFrequency(3);
+        int minOps = INT_MAX;
+        for(int row = 0; row < size; ++row){
+            for(int col = 0; col < size; ++col){
+                if(isOnY(size, row, col, center))
+                    yFrequency[grid[row][col]]++;
+                else
+                    nonYFrequency[grid[row][col]]++;
+            }
+        }
+        for(int y = 0; y < 3; ++y){
+            for(int ny = 0; ny < 3; ++ny){
+                if(y == ny)
+                    continue;
+                minOps = min(minOps, ySize - yFrequency[y] + rest - nonYFrequency[ny]);
+            }
+        }
+        return minOps;
+    }
+};
