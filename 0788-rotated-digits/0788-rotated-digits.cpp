480935ce4e5ac3d5e820e5a82c4c4967889c025d
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2025-12-03 10:00:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (89.06%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,21 +1,26 @@
 class Solution {
     vector<int> mirror = {0, 1, 5, -1, -1, 2, 9, -1, 8, 6};
     string numStr;
+    int memo[6][2][2];
 public:
     int countDP(int pos, bool isLast, bool mirrored){
         if(pos == numStr.size())
             return mirrored;
-        int start = 0;
-        int end = isLast ? numStr[pos] - '0' : 9;
-        int count = 0;
-        for(; start <= end; ++start){
-            if(mirror[start] == -1)
-                continue;
-            count += countDP(pos + 1, isLast && (start == end), mirrored || (mirror[start] != start));
+        int& count = memo[pos][isLast][mirrored];
+        if(count == -1){
+            count = 0;
+            int start = 0;
+            int end = isLast ? numStr[pos] - '0' : 9;
+            for(; start <= end; ++start){
+                if(mirror[start] == -1)
+                    continue;
+                count += countDP(pos + 1, isLast && (start == end), mirrored || (mirror[start] != start));
+            }
         }
         return count;
     }
     int rotatedDigits(int n) {
+        memset(memo, -1, sizeof(memo));
         numStr = to_string(n);
         return countDP(0, true, 0);
     }
