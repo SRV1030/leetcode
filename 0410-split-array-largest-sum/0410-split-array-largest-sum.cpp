673b5140d7fa336f10719cc7d68cf8267ac85978
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-01-05 07:47:29 +0530

    Time: 0 ms (100%), Space: 10.4 MB (56.97%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool checkMaxSum(vector<int>& nums, int k, int maxSum){
-        int sum = 0;
-        int subArrays = 0;
+    bool check(vector<int>& nums,int mid,int k){
+        long long sum = 0, splits = 0;
         for(auto& num: nums){
             sum += num;
-            if(sum > maxSum){
-                ++subArrays;
+            if(sum > mid){
                 sum = num;
+                ++splits;
             }
-            if(subArrays > k)
+            if(splits >= k)
                 return false;
         }
-        return subArrays < k;
+        return splits < k;
     }
     int splitArray(vector<int>& nums, int k) {
-        int low = 0, high = 0;
-        for(auto& num: nums){
-            low = max(num, low);
+        long long low = 0, high = 0;
+        for(auto& num : nums){
+            low = max(low, 1ll * num);
             high += num;
         }
         while(low < high){
-            int mid = (low + high) / 2;
-            if(checkMaxSum(nums, k, mid))
+            long long mid = (low + high) / 2;
+            if(check(nums, mid, k))
                 high = mid;
-            else low = mid + 1;
+            else 
+                low = mid + 1;
         }
         return low;
     }
