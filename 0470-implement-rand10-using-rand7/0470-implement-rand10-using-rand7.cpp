b73c9a7818c9ff2ac4dabba3a45dcff4a1325dc1
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-01-31 13:59:41 +0530

    Time: 9 ms (38.64%), Space: 10.7 MB (92.15%) - LeetHub

---
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.cpp
@@ -0,0 +1,17 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        while(true){
+            int a = rand7();
+            int b = rand7();
+            int num = (a - 1) * 7 + b;
+            if(num <= 40)
+                return (num - 1) % 10 + 1;
+        }
+        return -1;
+    }
+};
