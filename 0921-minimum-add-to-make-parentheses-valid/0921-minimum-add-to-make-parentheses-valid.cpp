2e557bd0f003f35fc96d77bf2b18e3f035135c61
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-02-01 14:37:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (56.7%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0, close = 0;
+        for(auto&i:s){
+            if(i=='(') ++open;
+            else {
+                if(open == 0) ++close;
+                else open--;
+            }
+        }
+        return open + close;
+    }
+};
