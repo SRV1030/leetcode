fb1c270f9f0300dcf37da358165e48be564a4c60
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-02-05 13:03:42 +0530

    Time: 2 ms (7.94%), Space: 6 MB (100%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1) return true;
+        long long int l=1, h=num/2;
+        while(l<=h){
+            long long int m=l+(h-l)/2,s=m*m;
+            if(s==num) return true;
+            if(s<num)l=m+1;
+            else h=m-1;            
+        }
+        return false;
+    }
+};
