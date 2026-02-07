1021f405ef2248165f15ce440cecc093c1331027
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-02-07 18:41:16 +0530

    Time: 4 ms (71.04%), Space: 8.6 MB (79.13%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void countPalindrome(int index, string& s, int offset, int& count){
-        int left = index, right = index + offset;
-        while(left >= 0 && right < s.size() && s[left--] == s[right++]){
-            ++count;
-        }
-    }
-    int countSubstrings(string& s) {
+    int countSubstrings(string s) {
         int count = 0;
         for(int index = 0; index < s.size(); ++index){
-            countPalindrome(index, s, 0, count);
-            countPalindrome(index, s, 1, count);
+            int left = index, right = index;
+            while(left >= 0 && right < s.size() && s[left--] == s[right++]){
+                ++count;
+            }
+            left = index, right = index + 1;
+            while(left >= 0 && right < s.size() && s[left--] == s[right++]){
+                ++count;
+            }
         }
         return count;
     }
