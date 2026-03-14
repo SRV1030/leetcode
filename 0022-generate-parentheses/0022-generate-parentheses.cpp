ead4b5366a25ca0c44bc5425d7e33051ba8e4711
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-03-14 20:26:14 +0530

    Time: 2 ms (81.11%), Space: 16.1 MB (25.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    vector<string> ans;
-    void generate(int o,int c,string t=""){
-        if(c==0)ans.push_back(t);
-        else{
-            if(c>o)generate(o,c-1,t+')');
-            if(o>0)generate(o-1,c,t+'(');
+    vector<string> result;
+    void fill(int open, int closed, string eqn){
+        if(!closed){
+            result.push_back(eqn);
+            return;
         }
+        if(open)
+            fill(open - 1, closed, eqn + "(");
+        if(open < closed)
+            fill(open, closed - 1, eqn + ")");
     }
     vector<string> generateParenthesis(int n) {
-        generate(n,n);
-        return ans;
+        fill(n, n, "");
+        return result;
     }
 };
