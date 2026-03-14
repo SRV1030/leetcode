36ac7a1ac5635081af02b602e095efd261d91d01
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-03-14 20:25:28 +0530

    Time: 21 ms (91.98%), Space: 16.4 MB (67.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,26 @@
 class Solution {
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int rows, cols;
 public:
-    int rowSize,colSize;
-    vector<vector<int>>directions= {{1,0},{0,1},{-1,0},{0,-1}};
-    void dfs(vector<vector<char>>& grid,int row,int col){
-        if(min(row,col)<0 || row>=rowSize || col>=colSize || grid[row][col]!='1') return;
-        grid[row][col] = '$';
-        for(auto&dir:directions)
-            dfs(grid,row+dir[0],col+dir[1]);
+    void dfs(int row, int col, vector<vector<char>>&  grid){
+        if(min(row, col) < 0 || row >= rows || col >= cols || grid[row][col] != '1')
+            return;
+        grid[row][col] = '0';
+        for(auto& dir : directions)
+            dfs(row + dir[0], col + dir[1], grid);
     }
     int numIslands(vector<vector<char>>& grid) {
-        rowSize = grid.size();
-        colSize = grid[0].size();
-        int islands = 0;
-        for(int row=0;row<rowSize;row++)
-            for(int col=0;col<colSize;col++)
-                if(grid[row][col]=='1'){
-                    dfs(grid,row,col);
-                    islands++;
+        rows = grid.size();
+        cols = grid[0].size();
+        int count = 0;
+        for(int row = 0; row < rows; ++row){
+            for(int col = 0; col < cols; ++col){
+                if(grid[row][col] == '1'){
+                    ++count;
+                    dfs(row, col, grid);
                 }
-        return islands;
+            }
+        }
+        return count;
     }
 };
