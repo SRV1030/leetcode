fae3b1583ed1a68722e9599c2fd01b333c975a17
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-04-05 14:29:46 +0530

    Time: 3 ms (46.4%), Space: 10.3 MB (47.26%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int x=0,y=0;
-        for(auto i:moves){
-            switch(i){
-                case 'L':
-                    x--;
-                    break;
-                case 'R':
-                    x++;
-                    break;
-                case 'U':
-                    y++;
-                    break;
-                case 'D':
-                    y--;
-                    break;
-                    
-            }
-        
-        }
-        return x==0&&y==0;
+        int x = 0, y = 0;
+        for(auto& ch : moves){
+            if(ch == 'U')
+                ++y;
+            if(ch == 'D')
+                --y;
+            if(ch == 'L')
+                --x;
+            if(ch == 'R')
+                ++x;
+        } 
+        return x == 0 && y == 0; 
     }
 };
