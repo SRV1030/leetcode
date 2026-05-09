0ec03c6be50e4be3d0f912676a232c9bc74269fc
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-05-09 16:06:26 +0530

    Time: 17 ms (36.03%), Space: 17.8 MB (42.65%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
-        int cnt=0,m=grid.size(),n=grid[0].size(),l=0,u=0,r=n-1,d=m-1;
-        while(cnt<m*n){
-            vector<int>arr;
-            for(int i=l;i<=r;i++,cnt++)arr.push_back(grid[u][i]);
-            for(int i=u+1;i<=d;i++,cnt++)arr.push_back(grid[i][r]);
-            for(int i=r-1;i>=l;i--,cnt++)arr.push_back(grid[d][i]);
-            for(int i=d-1;i>u;i--,cnt++)arr.push_back(grid[i][l]);
-            int s=arr.size(),c=0;
-            for(int i=l;i<=r;i++,c++)grid[u][i]=arr[(k+c)%s];
-            for(int i=u+1;i<=d;i++,c++)grid[i][r]=arr[(k+c)%s];
-            for(int i=r-1;i>=l;i--,c++)grid[d][i]=arr[(k+c)%s];
-            for(int i=d-1;i>u;i--,c++)grid[i][l]=arr[(k+c)%s];
-            l++;
-            u++;
-            d--;
-            r--;
+        int left = 0, top = 0, bottom = grid.size() - 1, right = grid[0].size() - 1;
+        while(left < right && top < bottom){
+            vector<int> layer;
+            for(int ind = left; ind <= right; ++ind)
+                layer.push_back(grid[top][ind]);
+            for(int ind = top + 1; ind <= bottom; ++ind)
+                layer.push_back(grid[ind][right]);
+            for(int ind = right - 1; ind >= left; --ind)
+                layer.push_back(grid[bottom][ind]);
+            for(int ind = bottom - 1; ind > top; --ind)
+                layer.push_back(grid[ind][left]);
+
+            int idx = 0;
+            int size = layer.size();
+
+            for(int ind = left; ind <= right; ++ind, ++idx)
+                grid[top][ind] = layer[(idx + k) % size];
+            for(int ind = top + 1; ind <= bottom; ++ind, ++idx)
+                grid[ind][right] = layer[(idx + k) % size];
+            for(int ind = right - 1; ind >= left; --ind, ++idx)
+                grid[bottom][ind] = layer[(idx + k) % size];
+            for(int ind = bottom - 1; ind > top; --ind, ++idx)
+                grid[ind][left] = layer[(idx + k) % size];
+            
+            ++left;
+            ++top;
+            --bottom;
+            --right;
         }
+
         return grid;
     }
 };
