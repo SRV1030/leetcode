77183650fb3f9bc2196660064ae5653a5154bf5e
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-05-11 09:35:45 +0530

    Time: 0 ms (100%), Space: 77.1 MB (43.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,32 +11,33 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* resultHead = new ListNode(0), *current = resultHead;
+        ListNode *sumHead = new ListNode(0), *sum = sumHead;
         int c = 0;
-        while(l1 && l2){
-            int sum = l1->val + l2->val + c;
-            c = sum / 10;
-            current->next = new ListNode(sum % 10);
-            current = current->next;
-            l1 = l1->next;
-            l2 = l2->next;
+        while(l1 || l2){
+            if(l1 && l2){
+                int val = l1->val + l2->val + c;
+                sum->next = new ListNode(val % 10);
+                c = val / 10;
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            else if(l1){
+                int val = l1->val + c;
+                sum->next = new ListNode(val % 10);
+                c = val / 10;
+                l1 = l1->next;
+            }
+            else {
+                int val = l2->val + c;
+                sum->next = new ListNode(val % 10);
+                c = val / 10;
+                l2 = l2->next;
+            }
+            sum = sum->next;
         }
-        while(l1){
-            int sum = l1->val + c;
-            c = sum / 10;
-            current->next = new ListNode(sum % 10);
-            current = current->next;
-            l1 = l1->next;
+        if(c){
+            sum->next = new ListNode(c);
         }
-        while(l2){
-            int sum = l2->val + c;
-            c = sum / 10;
-            current->next = new ListNode(sum % 10);
-            current = current->next;
-            l2 = l2->next;
-        }
-        if(c)
-            current->next = new ListNode(c);
-        return resultHead->next;
+        return sumHead->next;
     }
 };
