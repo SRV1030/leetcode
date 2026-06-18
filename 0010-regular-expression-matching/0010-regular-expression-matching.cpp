debf6062597072e798500f1750df7258207786cb
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-06-18 13:51:00 +0530

    Time: 19 ms (12.11%), Space: 23.3 MB (5.02%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool matchPattern(int i, int j, string s, string p) {
-        if (j == p.length())
-            return i == s.length();
-        int &ans= dp[i][j];
-        if (ans != -1)
-            return ans;
-        bool firstMatch = i<s.size() && (s[i] == p[j] || p[j] == '.');
-        if (j+1<p.size() && p[j+1] == '*')
-            return ans = matchPattern(i, j + 2, s, p) ||
-                  (firstMatch && matchPattern(i + 1, j, s, p));
-        else
-            return ans = firstMatch && matchPattern(i + 1, j + 1, s, p);
+    vector<vector<int>> memory;
+    int sizeOfS, sizeOfP;
+    bool isRegexMatch(string s, string p, int indexOfS, int indexOfP){
+        if(indexOfP == sizeOfP)
+            return indexOfS == sizeOfS;
+        if(indexOfS > sizeOfS || indexOfP > sizeOfP)
+            return false;
+        int& match = memory[indexOfS][indexOfP];
+        if(match == -1){
+            bool firstMatch = s[indexOfS] == p[indexOfP] || p[indexOfP] == '.';
+            if(indexOfP + 1 < sizeOfP && p[indexOfP + 1] == '*'){
+                bool skip = isRegexMatch(s, p, indexOfS, indexOfP + 2);
+                bool take = firstMatch && isRegexMatch(s, p, indexOfS + 1, indexOfP);
+                match = skip || take;
+            }
+            else
+                match = firstMatch && isRegexMatch(s, p, indexOfS + 1, indexOfP + 1);
+        }
+        return match;
     }
     bool isMatch(string s, string p) {
-        dp.resize(s.size() + 1, vector<int>(p.size() + 1, -1));
-        return matchPattern(0, 0, s, p);
+        sizeOfS = s.size();
+        sizeOfP = p.size();
+        memory = vector<vector<int>> (sizeOfS + 1, vector<int>(sizeOfP + 1, -1));
+        return isRegexMatch(s, p, 0, 0);
     }
 };
