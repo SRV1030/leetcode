b1fc9aab71ca691d89073d25c051626b2d692a45
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-07-07 08:06:50 +0530

    Time: 8 ms (68.16%), Space: 9.4 MB (83.59%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,31 +1,31 @@
 class Solution {
     int memo[301][27][27];
 public:
-    pair<int, int> getpos(int ind){
-        return {ind / 6, ind % 6};
+    pair<int, int> getPos(int ch){
+        return {ch / 6, ch % 6};
     }
-    int dist(int f, int ch){
+
+    int getDist(int f, int ch){
         if(f == 26 || ch == 26)
             return 0;
-        auto[x1, y1] = getpos(f);
-        auto[x2, y2] = getpos(ch);
-        return abs(x1 - x2) + abs(y1 -y2);
+        auto[x1, y1] = getPos(f);
+        auto[x2, y2] = getPos(ch);
+        return abs(x1 - x2) + abs(y1 - y2);
     }
-    int solve(int ind, int f1, int f2, string& word){
+
+    int solve(string &word, int f1, int f2, int ind){
         if(ind == word.size())
             return 0;
-        
-        int& res = memo[ind][f1][f2];
+        int &res = memo[ind][f1][f2];
         if(res == -1){
             int ch = word[ind] - 'A';
-            int m1 = dist(ch, f1) + solve(ind + 1, ch, f2, word);
-            int m2 = dist(ch, f2) + solve(ind + 1, f1, ch, word);
-            res = min(m1, m2);
+            res = min(getDist(f1, ch) + solve(word, ch, f2, ind + 1), 
+                        getDist(f2, ch) + solve(word, f1, ch, ind + 1));
         }
         return res;
     }
     int minimumDistance(string word) {
         memset(memo, -1, sizeof(memo));
-        return solve(0, 26, 26, word);
+        return solve(word, 26, 26, 0);
     }
 };
