d4656d6fc983f88193c4ec219ae29db4e6600a98
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-07-21 16:54:22 +0530

    Time: 4 ms (93.79%), Space: 29.3 MB (90.86%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestBeautifulSubstring(string w) {
+        int res = 0;
+        for (int i = 0, j = 0; i < w.size(); ++i) {
+            if (w[i] == 'a') {
+                int cnt = 0;
+                for (j = i + 1; j < w.size() && w[j - 1] <= w[j]; ++j)
+                    cnt += w[j - 1] < w[j];
+                if (cnt == 4)
+                    res = max(res, j - i);
+                i = j - 1;
+            }
+        }
+        return res;
+    }
+};
