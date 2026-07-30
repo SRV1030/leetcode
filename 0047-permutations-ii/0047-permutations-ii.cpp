5bcd46543723edab4dc5c57c7be3e01b2635b3db
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-07-30 17:21:16 +0530

    Time: 5 ms (41.02%), Space: 13.8 MB (27.48%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -2,22 +2,22 @@ class Solution {
     vector<vector<int>> result;
     int size;
 public:
-    void generate(vector<int>& nums, int low){
+    void generate(vector<int> nums, int low){
         if(low == size - 1){
             result.push_back(nums);
             return;
         }
-        set<int> seen;
         for(int ind = low; ind < size; ++ind){
-            if(seen.count(nums[ind]))
+            if(ind != low && nums[ind] == nums[low])
                 continue;
             swap(nums[ind], nums[low]);
             generate(nums, low + 1);
-            swap(nums[ind], nums[low]);
-            seen.insert(nums[ind]);
+            // swap(nums[ind], nums[low]);
+            // seen.insert(nums[ind]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         size = nums.size();
         generate(nums, 0);
         return result;
