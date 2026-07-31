bac326d96167a496b83717b7ff6668c6e8d7baa4
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-07-31 21:23:29 +0530

    Time: 0 ms (100%), Space: 8.8 MB (83.78%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int numSteps(string s) {
-        int carry = 0, steps = 0, sizeOfS = s.size();
-        for(int index = sizeOfS - 1; index > 0; index--){
-            ++steps;
-            if(s[index] + carry - '0' == 1){
+        int count = 0, carry = 0;
+        for(int ind = s.size() - 1; ind > 0; --ind){
+            int bit = s[ind] - '0';
+            ++count;
+            if(bit + carry == 1){
+                ++count; 
                 carry = 1;
-                ++steps;
             }
         }
-        return steps + carry;
+        return count + carry;
     }
 };
