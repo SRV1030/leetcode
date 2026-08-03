4ada36508691a067f63322deedf92bf7a97aaa51
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-08-03 10:45:35 +0530

    Time: 187 ms (54.79%), Space: 64.2 MB (38.02%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,61 +1,62 @@
-struct Answer {
-    array<int, 3> ans{};
-
-    void put(int x) {
-        if (x > ans[0]) {
-            tie(ans[0], ans[1], ans[2]) = tuple{x, ans[0], ans[1]};
-        } else if (x != ans[0] && x > ans[1]) {
-            tie(ans[1], ans[2]) = tuple{x, ans[1]};
-        } else if (x != ans[0] && x != ans[1] && x > ans[2]) {
-            ans[2] = x;
-        }
-    }
-
-    vector<int> get() const {
-        vector<int> ret;
-        for (int num : ans) {
-            if (num) {
-                ret.push_back(num);
-            }
-        }
-        return ret;
-    }
-};
-
 class Solution {
 public:
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> sum1(m + 1, vector<int>(n + 2));
-        vector<vector<int>> sum2(m + 1, vector<int>(n + 2));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                sum1[i][j] = sum1[i - 1][j - 1] + grid[i - 1][j - 1];
-                sum2[i][j] = sum2[i - 1][j + 1] + grid[i - 1][j - 1];
-            }
-        }
-        Answer ans;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                // a single cell is also a rhombus
-                ans.put(grid[i][j]);
-                for (int k = i + 2; k < m; k += 2) {
-                    int ux = i, uy = j;
-                    int dx = k, dy = j;
-                    int lx = (i + k) / 2, ly = j - (k - i) / 2;
-                    int rx = (i + k) / 2, ry = j + (k - i) / 2;
-                    if (ly < 0 || ry >= n) {
-                        break;
-                    }
-                    ans.put((sum2[lx + 1][ly + 1] - sum2[ux][uy + 2]) +
-                            (sum1[rx + 1][ry + 1] - sum1[ux][uy]) +
-                            (sum1[dx + 1][dy + 1] - sum1[lx][ly]) +
-                            (sum2[dx + 1][dy + 1] - sum2[rx][ry + 2]) -
-                            (grid[ux][uy] + grid[dx][dy] + grid[lx][ly] +
-                             grid[rx][ry]));
+
+        // ↘ diagonal prefix
+        vector<vector<int>> diag1(m + 1, vector<int>(n + 1, 0));
+
+        // ↙ diagonal prefix
+        vector<vector<int>> diag2(m + 1, vector<int>(n + 2, 0));
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                diag1[i + 1][j + 1] = diag1[i][j] + grid[i][j];
+
+        for (int i = 0; i < m; i++)
+            for (int j = n - 1; j >= 0; j--)
+                diag2[i + 1][j] = diag2[i][j + 1] + grid[i][j];
+
+        set<int, greater<int>> ans;
+
+        // radius = 0
+        for (auto &row : grid)
+            for (int x : row)
+                ans.insert(x);
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+
+                for (int k = 1;
+                     r + 2 * k < m &&
+                     c - k >= 0 &&
+                     c + k < n;
+                     k++) {
+
+                    // Four edges (corners included)
+                    int e1 = diag1[r + k + 1][c + k + 1] - diag1[r][c];
+                    int e2 = diag2[r + 2 * k + 1][c] - diag2[r + k][c + k + 1];
+                    int e3 = diag1[r + 2 * k + 1][c + 1] - diag1[r + k][c - k];
+                    int e4 = diag2[r + k + 1][c - k] - diag2[r][c + 1];
+
+                    int sum = e1 + e2 + e3 + e4;
+
+                    // remove duplicated corners
+                    sum -= grid[r][c];
+                    sum -= grid[r + k][c + k];
+                    sum -= grid[r + 2 * k][c];
+                    sum -= grid[r + k][c - k];
+
+                    ans.insert(sum);
                 }
             }
         }
-        return ans.get();
+
+        vector<int> res;
+        for (int x : ans) {
+            res.push_back(x);
+            if (res.size() == 3) break;
+        }
+        return res;
     }
 };
