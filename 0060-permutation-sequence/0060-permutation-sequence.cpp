022a58f6b4e3459c422f90b49f79f162b58eee5a
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-08-11 13:09:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (17.38%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        int fact = 1;
-        vector<int> perm0;
-        for (int i = 1; i < n; i++) {
-            fact *= i;
-            perm0.push_back(i);
+        int blockSize = 1;
+        vector<int> perm;
+        string res = "";
+        for(int index = 1; index < n; ++index){
+            blockSize *= index;
+            perm.push_back(index);
         }
+        perm.push_back(n);
         --k;
-        perm0.push_back(n);
-        string permK = "";
-        while(!perm0.empty()){
-            int ind = k/fact;
-            permK.push_back(perm0[ind]+'0');
-            perm0.erase(perm0.begin()+ind);
-            if(perm0.empty()) break;
-            k%=fact;
-            fact/=perm0.size();
+        while(!perm.empty()){
+            int index = k / blockSize;
+            res += to_string(perm[index]);
+            perm.erase(perm.begin() + index);
+            if(perm.empty())
+                return res;
+            k %= blockSize;
+            blockSize /= perm.size();
         }
-        return permK;
+        return res;
     }
 };
