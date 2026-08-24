07ddab21d4b471473fa42ebe07ccbc8cc2f75870
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2026-08-24 14:25:39 +0530

    Time: 7 ms (93.63%), Space: 12.8 MB (89.48%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        queue<vector<int>> bfsQueue;
-        set<vector<int>> visited;
-        int nodes = graph.size();
-        int allPos = (1 << nodes) - 1;
-        for(int node = 0; node < nodes; ++node){
+        int n = graph.size();
+        int allMask = (1 << n) - 1;
+        vector<vector<bool>> visited(n, vector<bool>(1 << n, false));
+        queue<pair<int, int>> q;
+        for (int node = 0; node < n; ++node) {
             int mask = 1 << node;
-            bfsQueue.push({node, 0, mask});
-            visited.insert({node, mask});
+            q.push({node, mask});
+            visited[node][mask] = true;
         }
-        while(!bfsQueue.empty()){
-            vector<int> state = bfsQueue.front();
-            bfsQueue.pop();
-            if(state[2] == allPos)
-                return state[1];
-            for(auto& nb : graph[state[0]]){
-                int mask = state[2] | (1 << nb);
-                if(visited.insert({nb, mask}).second){
-                    bfsQueue.push({nb, state[1] + 1, mask});
+
+        int dist = 0;
+
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz--) {
+                auto [node, mask] = q.front();
+                q.pop();
+
+                if (mask == allMask)
+                    return dist;
+
+                for (int nb : graph[node]) {
+                    int newMask = mask | (1 << nb);
+
+                    if (!visited[nb][newMask]) {
+                        visited[nb][newMask] = true;
+                        q.push({nb, newMask});
+                    }
                 }
             }
+
+            ++dist;
         }
+
         return -1;
     }
 };
